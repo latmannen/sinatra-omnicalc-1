1e0a6a2e49a36e6788c4f056098ba53894f29558
latmannen <163900633+latmannen@users.noreply.github.com>
2024-04-16 19:45:06 +0000

    mostly finished up payments section

--- app.rb
+++ app.rb
@@ -1,10 +1,12 @@
 require "sinatra"
 require "sinatra/reloader"
 
+#Homepage
 get("/") do
   erb(:square)
 end
 
+#Square Section
 get("/square/new") do
   erb(:square)
 end
@@ -15,6 +17,7 @@ get("/square/results") do
   erb(:square_results)
 end
 
+#Square Root Section
 get("/square_root/new") do
   erb(:square_root)
 end
@@ -23,4 +26,18 @@ get("/square_root/results") do
   @the_num = params.fetch("users_number").to_f
   @the_result = @the_num ** 0.5
   erb(:square_root_results)
+end
+
+#Payment Section. Follow the rake grade to see what to do next. mostly formatting. 
+get("/payment/new") do
+  erb(:payment)
+end
+
+get("/payment/results") do
+  @apr = params.fetch("users_apr").to_f
+  @years = params.fetch("users_years").to_f
+  @pv = params.fetch("users_pv").to_f
+  
+  @the_result = (@apr/100/12 * @pv)/(1-(1+@apr/100/12)** (-1.0 * @years*12))
+  erb(:payment_results)
 end
